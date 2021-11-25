58a4ed601cab9def72d5d24fad1c7dcc99236ad9
popetrov <popetrov64@gmail.com>
2021-11-25 15:56:03 +0400

    study state and developments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Lesson } from './components/Lesson';
 
 function App() {
--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, MouseEvent, useState } from 'react'
 
 export type CounterTextProps = {
     counter: number,
@@ -8,28 +8,85 @@ export type CounterTextProps = {
 }
 
 const CounterText:FC <CounterTextProps> = ({counter, func, number, string}) =>{
-    console.log(counter, func, number, string)
+    if(counter<5){
     return(
-        <h1>Counter value: {counter}</h1>
-    )
+        <h2>Counter value: {counter}</h2>
+    )}else{
+        return(
+            <h1>Big Counter value: {counter} + Very Good</h1>
+        )
+    }
 }
+
+type MyLinkProps = {
+    text:number
+}
+
+
 
 export const Lesson = ()=>{
     const [state, setState] = useState(0)
 
-    const handleClick = () =>{
-        setState(state+1)
+    const MyLink:FC<MyLinkProps>=({text})=>{
+        return(
+            <a href="/test" onClick={onclick}>{text}</a>
+        )
     }
+    
+    const onclick = (e: { preventDefault: () => void })=>{
+        e.preventDefault()
+        setState(Number(state)+3)
+        
+    }
+
+    const handleClick = () =>{
+        setState(Number(state)+1)
+    }
+
+    const handleTab = (e: any)=>{
+        setState(e.target.getAttribute('data-name'))
+    }
+
+    const TABS_BTN = [
+        {
+            dataName:1,
+            title:'Counter 1'
+        },
+        {
+            dataName:2,
+            title:'Counter 2'
+        },
+        {
+            dataName:3,
+            title:'Counter 3'
+        },
+    ]
+    
+    const people = ['Jack', 'Alex', 'John', 'Bill']
 
     return(
         <div>
+            <MyLink text={state}/>
             <CounterText 
                 counter={state}
                 func={()=>{}}
                 number={1}
                 string={`string`} 
-        />
-            <button onClick={handleClick}>Counter Plus</button>
+            />
+            <button onClick={handleClick}>Counter Plus 1</button>
+            {TABS_BTN.map(({title, dataName})=>{
+                return <button
+                    key={`${dataName}-${title}`}
+                    data-name={dataName}
+                    onClick={handleTab}
+                >{title}</button>
+            })}
+            <h3>This is TAB{state}</h3>
+            <ul>
+                {people.map((person, index)=>{
+                   return <li key={index}>{person}</li>
+                })}
+            </ul>
         </div>
     )
 }
