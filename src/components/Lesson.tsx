c90aebe78bbcc689f1eb42fb1251e19539792802
popetrov <popetrov64@gmail.com>
2021-11-30 13:08:08 +0400

    added ref

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -2,8 +2,8 @@ import React, { FC, Fragment, useState} from 'react'
 import './style.css'
 
 export const Form = ()=>{
-    const [inputText, setInputText] = useState([])
-    const[textAreaText, setTextAreaText] = useState([])
+    const [inputText, setInputText] = useState('')
+    const[textAreaText, setTextAreaText] = useState('')
     const[click, setClick] = useState({name:'', text:'', favoriteName:''})
     const[selectText, setSelectText] = useState("выберите любимое имя")
 
@@ -22,35 +22,34 @@ export const Form = ()=>{
         }
     ]
 
-    const inputHandleChange = (e:any)=>{
-        setInputText(e.target.value)
+    const handleChange = ()=>{
+        setInputText(inputRef.current!.value)
+        setTextAreaText(textAreaRef.current!.value)
+        setSelectText(selectRef.current!.value)
     }
 
-    const textAreaHandleChange = (e:any)=>{
-        setTextAreaText(e.target.value)
-    }
-
+    const inputRef = React.createRef<HTMLInputElement>()
+    const textAreaRef = React.createRef<HTMLTextAreaElement>()
+    const selectRef = React.createRef<HTMLSelectElement>()
+   
     const clickHandle = (e:any)=>{
         e.preventDefault()
         setClick({name:`${inputText}`, text:`${textAreaText}`, favoriteName:`${selectText}`})
         console.log(click)
     }
 
-    const selectHandleChange = (e:any)=>{
-        setSelectText(e.target.value)
-    }
 
     return(
         <Fragment>
             <form action="" className="form">
                 <label htmlFor="text">
                     name:
-                    <input type="text" name="name" value={inputText} onChange={inputHandleChange}/>
+                    <input ref={inputRef} type="text" name="name" value={inputText} onChange={handleChange}/>
                 </label>
                 <label htmlFor="text">Comment</label>
-                <textarea id="text" value={textAreaText} onChange={textAreaHandleChange}></textarea>
+                <textarea ref={textAreaRef} id="text" value={textAreaText} onChange={handleChange}></textarea>
                 <button onClick={clickHandle}>Click</button>
-                <select value={selectText} onChange={selectHandleChange}>
+                <select ref={selectRef} value={selectText} onChange={handleChange}>
                     <option value="liked name" ></option>
                     {NAME.map(({id,title})=>(
                         <option id={id+title}>{title}</option>
